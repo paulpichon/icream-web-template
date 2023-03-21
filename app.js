56646d001581e9d51878b3e5cc2d417623150e55
paul pichon <paul10_barca@hotmail.com>
2023-03-21 17:09:35 -0600

    Creacion pagina 404, sendfile

--- app.js
+++ app.js
@@ -7,10 +7,32 @@ const port = 8080;
 //static files
 app.use(express.static('public'));
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+//about
+app.get('/about', (req, res) => {
+  res.sendFile( __dirname + '/public/about.html' );
+});
+//contact
+app.get('/contact', (req, res) => {
+  res.sendFile( __dirname + '/public/contact.html' );
+});
+//gallery
+app.get('/gallery', (req, res) => {
+  res.sendFile( __dirname + '/public/gallery.html' );
+});
+//product
+app.get('/product', (req, res) => {
+  res.sendFile( __dirname + '/public/product.html' );
+});
+//service
+app.get('/service', (req, res) => {
+  res.sendFile( __dirname + '/public/service.html' );
+});
+//error 404
+app.get('/*', (req, res) => {
+  res.sendFile( __dirname + '/public/404.html' );
 });
 
+//puerto
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 });
